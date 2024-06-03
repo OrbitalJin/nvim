1e6024d122542fd8ed4adcce542b397506a01050
HorizonLabs <44175745+QuaeriteVeritatis@users.noreply.github.com>
2024-06-03 13:12:28 +0100

    Moved to main

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,27 @@
+-- My lspconfig
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- Define the servers
+local servers = { 
+  "html", 
+  "cssls", 
+  "tailwindcss", 
+  "tsserver", 
+  "eslint", 
+  "pyright", 
+  "gopls",
+}
+
+-- Iterate over servers and init their respective lsps 
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
+end
+
