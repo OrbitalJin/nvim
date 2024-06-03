241143d5d9994a39604a601ee52f0d874400e137
HorizonLabs <44175745+QuaeriteVeritatis@users.noreply.github.com>
2024-06-03 14:05:46 +0100

    Python venv picker

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,6 +1,22 @@
 -- These are my custom plugins
 local plugins = {
   {
+    "linux-cultist/venv-selector.nvim",
+      dependencies = {
+        "neovim/nvim-lspconfig", 
+        "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+        { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+      },
+    lazy = false,
+    branch = "regexp", -- This is the regexp branch, use this for the new version
+    config = function()
+        require("venv-selector").setup()
+      end,
+      keys = {
+        { ",v", "<cmd>VenvSelect<cr>" },
+      },
+  },
+  {
     "nvimtools/none-ls.nvim",
     event = "VeryLazy",
     opts = function()
@@ -25,6 +41,7 @@ local plugins = {
     opts = {
       ensure_installed = {
         "typescript-language-server",
+        "lua-language-server",
         "tailwindcss-language-server",
         "eslint-lsp",
         "prettierd",
